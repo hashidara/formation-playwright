5ae70f5008c5371fabdcbd9fcdf11434dc23b26d
Robin MANSUY <r.mansuy@younup.fr>
2026-09-25 15:08:52 +0200

    New changefix storageState

--- tests/secure-area.spec.js
+++ tests/secure-area.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('already logged in via storageState', async ({ page }) => {
+test('already logged in via storageState', async ({ page, browserName }) => {
   test.skip(browserName !== 'chromium', 'Session liée au User-Agent sur ce site, setup fait avec UA Chrome uniquement');
   
   await page.goto('https://the-internet.herokuapp.com/secure');
